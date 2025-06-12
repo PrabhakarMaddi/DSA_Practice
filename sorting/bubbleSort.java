409291066bb98abe0319a063efbae07f48e2076d
Prabha_boii <160480476+PrabhakarMaddi@users.noreply.github.com>
2025-06-12 10:43:50 +0530

    Create bubbleSort.java

---
+++ sorting/bubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+class Main {
+    public static void swap(int[] arr,int a,int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    public static int[] bubbleSort(int[] arr){
+        int n = arr.length;
+        for(int i=0 ; i<n-1 ; i++ ){
+            for(int j=i+1 ; j<n ; j++){
+                if(arr[j] < arr[i])
+                 swap(arr,i,j);
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+       int[] arr = {23,45,1,34,12,56,9};
+       int[] sortedArray = bubbleSort(arr);
+       System.out.println(Arrays.toString(sortedArray));
+    }
+}
